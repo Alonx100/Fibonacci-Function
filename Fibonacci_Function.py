bbbcca01809323fecaccbe71607643b2e31bb899
Administrator <strayer_practice@outlook.com>
2023-02-07 02:03:53 -0800

    Hello

--- Fibonacci_Function.py
+++ Fibonacci_Function.py
@@ -16,10 +16,16 @@ num1 = 1
 
 def function(num0, num1):
     for num in range(16):
-        print(num0)
+        print(num0, end = ",")
         result = num0 + num1
         num0 = num1
         num1 = result
+   
     return result
 
+
+
+
 function(num0, num1)
+
+
