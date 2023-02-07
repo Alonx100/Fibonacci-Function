ec5d6a1b80f7bcfa776b8a123d5525531fe3f35a
Administrator <strayer_practice@outlook.com>
2023-02-07 02:06:39 -0800

    Hello

--- Fibonacci_Function.py
+++ Fibonacci_Function.py
@@ -3,16 +3,6 @@ print("Fibonacci series for 16:", )
 num0 = 0
 num1 = 1
 
-#def function(n):
-
- #   terms = 16
-  #  while terms > 0:
-   #     terms -= 1
-    #    result = function(n-1)+function(n-2)
-    #    return result
-
-   # else:
-        #print("done")
 
 def function(num0, num1):
     for num in range(16):
@@ -22,8 +12,6 @@ def function(num0, num1):
         num1 = result
    
     return result
-
-
 
 
 function(num0, num1)
